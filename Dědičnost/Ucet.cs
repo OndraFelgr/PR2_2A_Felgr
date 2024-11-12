76c84d0dd17c80513f5ea37960fa837a17309a1c
OndraFelgr <ondrej.felgr@gmail.com>
2024-11-12 09:37:24 +0100

    Add project files.

---
+++ Dědičnost/Ucet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dědičnost
+{
+    internal class Ucet
+    {
+        public double Stav { get; private set; }
+
+        public Ucet(double pocatecniStav = 0)
+        {
+            if (pocatecniStav < 0)
+            {
+                throw new ArgumentException("Počáteční stav nemůže být záporný.");
+            }
+            Stav = pocatecniStav;
+        }
+    }
+}
