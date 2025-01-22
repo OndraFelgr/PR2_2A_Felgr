b8eb32058d2fc5ab3068adbfde6dc2acde55a84b
OndraFelgr <ondrej.felgr@gmail.com>
2025-01-22 07:55:57 +0100

    test

---
+++ TESTOPAKOVANI/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TESTOPAKOVANI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cisla = { 1.3, 2.2, -1.5, 1.4, -2.7 };
+            Console.WriteLine(PrumerNadLimitem(cisla, 1.1));
+            Console.WriteLine(PrumerNadLimitem(cisla, -2));
+            Console.WriteLine(PrumerNadLimitem(cisla, -3));
+
+            
+            double PrumerNadLimitem(double[] cisla, double limit)
+            {
+                double soucetCisel = 0;
+                int pocetCisel = 0;
+                for (int i = 0; i > cisla.Length; i++)
+                {
+                    if (cisla[i] < limit)
+                    {
+                        soucetCisel += cisla[i];
+                        pocetCisel++;
+                    }
+
+                }
+                return soucetCisel / pocetCisel;
+            }
+        }
+       
+    }
+}
---
+++ TestPoMaximu/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace TestPoMaximu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vstup;
+            int cislo;
+            
+            int nejvetsiCislo = Math.Min;
+            List<int> list = new List<int>();
+            
+            Console.WriteLine(string.Join(", ", list)); 
+            while (true)
+            {
+                Console.WriteLine("napis cele cislo");
+                vstup = Console.ReadLine();
+
+                if(vstup == "dost")
+                {
+                    break;
+                }
+
+                else if(int.TryParse(vstup, out cislo))
+                {
+                    if(cislo > nejvetsiCislo)
+                    {
+                        cislo += 
+                        nejvetsiCislo = cislo;
+
+
+                        
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("nebylo vloženo žádné číslo");
+                }
+
+
+            }
+            Console.WriteLine("");
+        }
+    }
+}
